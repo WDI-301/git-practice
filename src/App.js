97b92b0d1cd05640b0399985a86ccc6ca3d238de
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-06-02 04:01:36 +0800

    Adding Section A

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ function App() {
       <header>
         <h1 style={{ backgroundColor: "green" }}>We Changed The Header</h1>
       </header>
-        <img src='https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png' className="App-logo" alt="logo" />
-        <h1>
-          Git practice
-        </h1>
-        <section><h3>Section 1</h3></section>
-        <section><h3>Section 2</h3></section>
-        <footer>This is a footer</footer>
+      <img
+        src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png"
+        className="App-logo"
+        alt="logo"
+      />
+      <h1>Git practice</h1>
+      <section> section 1 added by developer A</section>
+      <section>
+        <h3>Section 1</h3>
+      </section>
+      <section>
+        <h3>Section 2</h3>
+      </section>
+      <footer>This is a footer</footer>
     </div>
   );
 }
