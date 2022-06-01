e15f045e2aa59bbd55cf53aa8529f3c1b3f35ee8
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-06-02 03:17:54 +0800

    Adding HEADER/1-A

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1 style={{backgroundColor: 'red'}}>This is the header</h1>
+        <h1 style={{ backgroundColor: "green" }}>We Changed The Header</h1>
       </header>
-        <img src='https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png' className="App-logo" alt="logo" />
-        <h1>
-          Git practice
-        </h1>
-        <section><h3>Section 1</h3></section>
-        <section><h3>Section 2</h3></section>
-      <footer style={{background: 'red'}}><h2>This is the footer of the page</h2></footer>
+      <img
+        src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png"
+        className="App-logo"
+        alt="logo"
+      />
+      <h1>Git practice</h1>
+      <section>
+        <h3>Section 1</h3>
+      </section>
+      <section>
+        <h3>Section 2</h3>
+      </section>
+      <footer style={{ background: "red" }}>
+        <h2>This is the footer of the page</h2>
+      </footer>
     </div>
   );
 }
