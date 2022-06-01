215413246363821745322d1182b31bad1c5a4733
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-06-01 15:26:18 -0400

    We changed the footer

--- src/App.js
+++ src/App.js
@@ -6,21 +6,13 @@ function App() {
       <header>
         <h1 style={{ backgroundColor: "green" }}>We Changed The Header</h1>
       </header>
-      <img
-        src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png"
-        className="App-logo"
-        alt="logo"
-      />
-      <h1>Git practice</h1>
-      <section>
-        <h3>Section 1</h3>
-      </section>
-      <section>
-        <h3>Section 2</h3>
-      </section>
-      <footer style={{ background: "red" }}>
-        <h2>This is the footer of the page</h2>
-      </footer>
+        <img src='https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png' className="App-logo" alt="logo" />
+        <h1>
+          Git practice
+        </h1>
+        <section><h3>Section 1</h3></section>
+        <section><h3>Section 2</h3></section>
+        <footer>This is a footer</footer>
     </div>
   );
 }
